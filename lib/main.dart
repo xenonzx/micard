7e3c5c352b1755e36e7b4b0afbbf25055d0334fa
Ahmed EL-Dakhakhny <>
2020-06-03 04:46:38 +0200

    lesson 1

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,19 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp();
+  runApp(MaterialApp(
+    home: Scaffold(
+      appBar: AppBar(
+        title: Text(
+          "this is dakhakhny",
+        ),
+      ),
+      body: Center(
+        child: Image(
+          image:
+              NetworkImage('https://source.unsplash.com/Y-joaXX7XCQ/1600x900'),
+        ),
+      ),
+    ),
+  ));
 }
