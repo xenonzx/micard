d0bf2bbb753d3ffdfd582a120e9b3d51b20daace
Ahmed EL-Dakhakhny <>
2020-06-07 04:03:51 +0200

    using staless widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,28 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
-    home: Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.blueGrey[700],
-        title: Text(
-          "this is dakhakhny",
+  runApp(MyApp());
+}
+
+// user widgets to use hot reload
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.blueGrey[700],
+          title: Text(
+            "this is dakhakhny",
+          ),
+        ),
+        backgroundColor: Colors.blueGrey[300],
+        body: Center(
+          child: Image(
+            image: AssetImage('images/diamond.png'),
+          ),
         ),
       ),
-      backgroundColor: Colors.blueGrey[300],
-      body: Center(
-        child: Image(
-          image: AssetImage('images/diamond.png'),
-        ),
-      ),
-    ),
-  ));
+    );
+  }
 }
