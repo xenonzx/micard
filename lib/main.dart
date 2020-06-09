39b81239e4b59946975bb7e6f0eb77ce43da2241
Ahmed EL-Dakhakhny <>
2020-06-09 07:44:25 +0200

    layout challenge

--- lib/main.dart
+++ lib/main.dart
@@ -5,22 +5,84 @@ void main() {
 }
 
 // user widgets to use hot reload
+// remember that container are used for common positioning , painting and sizing
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.blueGrey[700],
-          title: Text(
-            "this is dakhakhny",
+      home: LayoutChanllenge(),
+    );
+  }
+}
+
+class LayoutChanllenge extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Container(
+          width: double.infinity,
+          height: double.infinity,
+          color: Colors.teal,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Container(
+                width: 100,
+                height: double.infinity,
+                color: Colors.red,
+              ),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    width: 100,
+                    height: 100,
+                    color: Colors.yellow,
+                  ),
+                  Container(
+                    width: 100,
+                    height: 100,
+                    color: Colors.green,
+                  ),
+                ],
+              ),
+              Container(
+                width: 100,
+                height: double.infinity,
+                color: Colors.blue,
+              ),
+            ],
           ),
         ),
-        backgroundColor: Colors.blueGrey[300],
-        body: Center(
-          child: Image(
-            image: AssetImage('images/diamond.png'),
-          ),
+      ),
+    );
+  }
+}
+
+class Home1 extends StatelessWidget {
+  const Home1({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.blueGrey[700],
+        title: Text(
+          "this is dakhakhny",
+        ),
+      ),
+      backgroundColor: Colors.blueGrey[300],
+      body: Container(
+        width: 200,
+        height: 200,
+        color: Colors.blue,
+        margin: EdgeInsets.all(10),
+        padding: EdgeInsets.all(10),
+        child: Image(
+          image: AssetImage('images/diamond.png'),
         ),
       ),
     );
